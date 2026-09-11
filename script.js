e533ae0f367ccf898da07cd8d87ced3f954702ad
Byr-git <258540064+Byr-git@users.noreply.github.com>
2026-09-10 23:42:59 -0500

    feat: agregar eliminación de tareas

--- script.js
+++ script.js
@@ -12,13 +12,28 @@ formulario.addEventListener('submit', (evento) => {
     }
 
     const tarea = document.createElement('li')
+    const btn_eliminar = document.createElement('button')
+
     tarea.textContent = texto
     lista_tareas.appendChild(tarea)
+
+    btn_eliminar.textContent = 'Eliminar'
+    tarea.appendChild(btn_eliminar)
+
     input_tarea.value = ''
+    input_tarea.focus()
 })
 
 lista_tareas.addEventListener('click', (evento) => {
+    if (evento.target.tagName === 'BUTTON') {
+        const tarea = evento.target.parentElement
+        tarea.remove()
+        return
+    }
+
     if (evento.target.tagName === 'LI') {
         evento.target.classList.toggle('completada')
-    }
+    } 
 })
+
+
